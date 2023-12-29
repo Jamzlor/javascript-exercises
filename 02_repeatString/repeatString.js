a07f0375835ae07a722dc14100bf47bfae80df81
Jamzlor <jameslo.dev@gmail.com>
2023-12-30 00:54:41 +0800

    Pass all test on repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+    let stringArr = [];
+    if (num < 0){
+        return 'ERROR';
+    } else {
+        for(let i = 0; i < num; i++){
+            stringArr.push(string);
+        }
+        return stringArr.join('');
+    }
 };
 
 // Do not edit below this line
