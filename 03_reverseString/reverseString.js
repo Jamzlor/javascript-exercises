ef97fd5187ba7c2165c9418cc143fc5acef7ffe3
Jamzlor <jameslo.dev@gmail.com>
2023-12-30 00:59:11 +0800

    Pass all test on reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let strArr = [];
+    for(i = str.length-1; i >= 0; i--){
+        strArr.push(str[i])
+    }
+    return strArr.join('');
 };
 
 // Do not edit below this line
