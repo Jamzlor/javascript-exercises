cba46c24822893cde3fc440dc1238be4504798a7
Jamzlor <jameslo.dev@gmail.com>
2024-01-04 19:33:14 +0800

    Pass all test on palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let mutatedStr = string.replace(/[\W]/g,'').toLowerCase();
+    let reverseStr = mutatedStr.split('').reverse().join('');
+    return (reverseStr === mutatedStr) ? true : false;
 };
 
 // Do not edit below this line
