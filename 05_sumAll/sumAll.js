377e463e41cb15f182aa680a28398d62581086bb
Jamzlor <jameslo.dev@gmail.com>
2024-01-04 16:48:39 +0800

    Pass all test in sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(startNum, endNum) {
+    let result = 0;
+    if(startNum >= 0 && endNum >= 0){
+        if(typeof startNum === 'number' && typeof endNum === 'number'){
+            if (startNum <= endNum) { 
+                for(let i = startNum; i <= endNum; i++){
+                result += i;
+                }
+            } else if(startNum > endNum){
+                for(let i = startNum; i >=endNum; i--){
+                    result += i;
+                }
+            }
+        } else {
+            return 'ERROR';
+        }     
+    } else {
+        return 'ERROR';
+    }
+  
+    
+    return result;
 };
 
 // Do not edit below this line
