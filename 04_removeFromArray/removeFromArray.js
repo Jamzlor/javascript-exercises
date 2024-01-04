e854acbd6a31b134bec5b07b653de961a34cf82c
Jamzlor <jameslo.dev@gmail.com>
2024-01-04 16:22:07 +0800

    Passed all test in removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...nums) {
+    for(let i = 0; i < nums.length; i++){
+        let indexOfNum = arr.indexOf(nums[i]);
+        if(indexOfNum >= 0){
+            arr.splice(indexOfNum, 1);
+        }  
+    }
+    return arr;
 };
 
 // Do not edit below this line
