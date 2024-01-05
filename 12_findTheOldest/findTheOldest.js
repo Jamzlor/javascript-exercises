0b8d4bdcb1f71e20aff17d553d530a5e7729c940
Jamzlor <jameslo.dev@gmail.com>
2024-01-06 01:38:41 +0800

    Pass all test on FindTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,31 @@
-const findTheOldest = function() {
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
 
+const findTheOldest = function(people) {
+    let arr = [];
+    const THIS_YEAR = new Date().getFullYear();
+
+    arr = people.map((people) => (people.hasOwnProperty('yearOfDeath') ? (people.yearOfDeath - people.yearOfBirth) : THIS_YEAR - people.yearOfBirth));
+    
+    let oldestAge = Math.max(...arr);
+    
+    return people[arr.indexOf(oldestAge)];
 };
 
+findTheOldest(people)
 // Do not edit below this line
 module.exports = findTheOldest;
