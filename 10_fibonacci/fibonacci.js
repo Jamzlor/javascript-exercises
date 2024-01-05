d81e06066db4b41eba04948809efdda52ba42561
Jamzlor <jameslo.dev@gmail.com>
2024-01-05 20:48:29 +0800

    Pass all test for fiboancci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let a = 1;
+    let b = 0;
+    let temp;
+    if(num >= 0){
+        for(let i = 1; i <= num; i++){
+            temp = a;
+            a = a + b;
+            b = temp;
+        }
+        return b;
+    } else {
+        return 'OOPS';
+    }
+    
 };
 
 // Do not edit below this line
